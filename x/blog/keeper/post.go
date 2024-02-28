ef56357fdee1e56876d2d2583135828ea23c8be8
Minh Trong <traminhtrong2604@gmail.com>
2024-02-28 10:16:59 +0700

    build: SetPostCount func in post.go

--- x/blog/keeper/post.go
+++ x/blog/keeper/post.go
@@ -37,3 +37,12 @@ func GetPostIDBytes(id uint64) []byte {
 	binary.BigEndian.PutUint64(bz, id)
 	return bz
 }
+
+func (k Keeper) SetPostCount(ctx sdk.Context, count uint64) {
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, []byte{})
+	byteKey := types.KeyPrefix(types.PostCountKey)
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, count)
+	store.Set(byteKey, bz)
+}

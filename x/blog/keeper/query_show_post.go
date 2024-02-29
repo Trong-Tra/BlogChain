d35658debe4930d0ac034e02d2008839a08e91c2
Minh Trong <traminhtrong2604@gmail.com>
2024-02-29 08:04:28 +0700

    build: show post function

--- x/blog/keeper/query_show_post.go
+++ x/blog/keeper/query_show_post.go
@@ -6,19 +6,21 @@ import (
 	"blog/x/blog/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (k Keeper) ShowPost(goCtx context.Context, req *types.QueryShowPostRequest) (*types.QueryShowPostResponse, error) {
 	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
+		return nil, status.Error(codes.InvalidArgument, "Invalid request")
 	}
-
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	post, found := k.GetPost(ctx, req.Id)
+	if !found {
+		return nil, sdkerrors.ErrKeyNotFound
+	}
 
-	return &types.QueryShowPostResponse{}, nil
+	return &types.QueryShowPostResponse{Post: &post}, nil
 }
